47e2b8c389d1effd3f8ad14a05e93e0448376afe
lekiagospel@gmail.com <lekiagospel@gmail.com>
2019-10-24 09:20:04 +0100

    unit test

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,3 +25,27 @@ $factory->define(User::class, function (Faker $faker) {
         'remember_token' => Str::random(10),
     ];
 });
+
+$factory->define(App\Thread::class, function ($faker){
+    return [
+        'user_id'=>function(){
+            return factory ('App\User')->create()->id;
+        },
+        'title'=>$faker->sentence,
+        'body'=>$faker->paragraph,
+    ];
+});
+
+$factory->define(App\Reply::class, function($faker){
+
+    return [
+        'thread_id'=>function(){
+            return factory('App\Thread')->create()->id;
+        },
+        'user_id'=>function(){
+            return factory('App\User')->create()->id;
+        },
+        'body'=>$faker->paragraph,
+    ];
+
+});
--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -3,18 +3,20 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function a_user_can_browse_threads()
     {
-        $response = $this->get('/');
+        $response = $this->get('/threads');
 
         $response->assertStatus(200);
     }
